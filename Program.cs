86ba4d7e6835ebcb8f9ed9356355491a65edcb20
Matan Salto <saltomtn9@gmail.com>
2022-12-29 13:46:40 +0200

    Add main class

---
+++ Program.cs
@@ -0,0 +1,13 @@
+
+using System;   
+using Omega_Sudoku.src.SudokuSolver;
+
+namespace Omega_Sudoku
+{
+    public class Program
+    {
+        public static void Main(string[] args) {            
+        }
+       
+    }
+}
