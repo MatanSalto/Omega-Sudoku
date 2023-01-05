1a8446bbc056c2fc751542e0348ada942b62b9d9
Matan Salto <saltomtn9@gmail.com>
2023-01-05 12:08:00 +0200

    Change class nanes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;   
+using Omega_Sudoku.src.SudokuSolver;
+
+namespace Omega_Sudoku
+{
+    public class Program
+    {
+        // public static void Main(string[] args) {            
+        // }
+       
+    }
+}
