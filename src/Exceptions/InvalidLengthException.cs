f6620d5bcdee5967839e6895f37d5220ee78fac5
Matan Salto <saltomtn9@gmail.com>
2023-01-10 14:35:33 +0200

    Add invalid length exception

--- src/Exceptions/InvalidLengthException.cs
+++ src/Exceptions/InvalidLengthException.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 
-public class InvalidLengthException : Exception
+namespace Omega_Sudoku.src.Exceptions
 {
-	
-	public InvalidLengthException(string message) : base(message) { }
-	public InvalidLengthException(int length) {
-		this("Invalid Length. " + length + " is not a perfect square");
-    }
+	public class InvalidLengthException : Exception
+	{
+
+		public InvalidLengthException(string message) : base(message) { }
+
+		public InvalidLengthException(int length) : this("Invalid Length. " + length + " is not a perfect square") { }
+	}
+
 }
+
