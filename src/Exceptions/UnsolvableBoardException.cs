4f020886d520e9bbddd16714b4c8a5efd6a144c9
Matan Salto <saltomtn9@gmail.com>
2023-01-10 14:36:41 +0200

    Add unsolvable board exception

--- src/Exceptions/UnsolvableBoardException.cs
+++ src/Exceptions/UnsolvableBoardException.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 
-public class UnsolvableBoardException : Exception
-{
-	public UnsolvableBoardException(string message)	: base(message)	{ }
 
+namespace Omega_Sudoku.src.Exceptions
+{
+    public class UnsolvableBoardException : Exception
+    {
+        public UnsolvableBoardException(string message) : base(message) { }
+
+    }
 }
+
