98d4ac3ac9b5fdb0490bfcfc85d3df0cd2fdc1bd
Matan Salto <saltomtn9@gmail.com>
2023-01-10 13:32:28 +0200

    Add invalid length exception

---
+++ src/Exceptions/InvalidLengthException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class InvalidLengthException : Exception
+{
+	
+	public InvalidLengthException(string message) : base(message) { }
+	public InvalidLengthException(int length) {
+		this("Invalid Length. " + length + " is not a perfect square");
+    }
+}
