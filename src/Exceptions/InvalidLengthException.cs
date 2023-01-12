f40bb2aeb621773011d3222917f4569896016a12
Matan Salto <saltomtn9@gmail.com>
2023-01-12 13:31:13 +0200

    Add documentation

--- src/Exceptions/InvalidLengthException.cs
+++ src/Exceptions/InvalidLengthException.cs
@@ -2,12 +2,26 @@
 
 namespace Omega_Sudoku.src.Exceptions
 {
+	/// <summary>
+	/// This class represents the Invalid Length Exception,
+	/// that is thrown when the user enters a string with an invalid
+	/// length (length that doesn't produce a valid sudoku board)
+	/// </summary>
 	public class InvalidLengthException : Exception
 	{
-
+		/// <summary>
+		/// Constructor for the InvalidLengthException class
+		/// </summary>
+		/// <param name="message">The string to output</param>
+		/// <returns></returns>
 		public InvalidLengthException(string message) : base(message) { }
 
-		public InvalidLengthException(int length) : this("Invalid Length. " + length + " is not a perfect square") { }
+		/// <summary>
+		/// Constructor for the InvalidLengthException class
+		/// </summary>
+		/// <param name="length">The invalid length of the string</param>
+		/// <returns></returns>
+		public InvalidLengthException(int length) : this("Invalid Length. Length " + length + " doesn't produce a valid sudoku board size") { }
 	}
 
 }
--- src/Exceptions/UnsolvableBoardException.cs
+++ src/Exceptions/UnsolvableBoardException.cs
@@ -3,8 +3,17 @@
 
 namespace Omega_Sudoku.src.Exceptions
 {
+    /// <summary>
+    /// This class represents the Unsolvable Board Exception,
+    /// that is thrown when the input board has no solution
+    /// </summary>
     public class UnsolvableBoardException : Exception
     {
+        /// <summary>
+        /// Constructor for the  UnsolvableBoardException
+        /// </summary>
+        /// <param name="message">The string to output</param>
+        /// <returns></returns>
         public UnsolvableBoardException(string message) : base(message) { }
 
     }
