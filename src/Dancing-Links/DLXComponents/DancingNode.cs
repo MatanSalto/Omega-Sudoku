48cbc1a5fff0af962ad60f8f7d71dc7222e8f256
Matan Salto <saltomtn9@gmail.com>
2023-01-12 13:27:49 +0200

    Fix imports and add documentation

--- src/Dancing-Links/DLXComponents/DancingNode.cs
+++ src/Dancing-Links/DLXComponents/DancingNode.cs
@@ -1,8 +1,6 @@
-using System.ComponentModel.Design.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Omega_Sudoku.src.DancingLinks
 {
@@ -14,13 +12,21 @@ namespace Omega_Sudoku.src.DancingLinks
         public DancingNode left, right, up, down;
         public ColumnHeaderNode header;
 
+        /// <summary>
+        /// Constructor for the DancingNode class
+        /// </summary>
+        /// <param name="header">The column header node</param>
         public DancingNode(ColumnHeaderNode header) {
-            // Initialize the linked nodes to null
+            // Initialize the connections to point at the current node
             this.left = this.right = this.up = this.down = this;
             // Set the column header node
             this.header = header;
         }
 
+        /// <summary>
+        /// This method links the given node to the right of the current node
+        /// </summary>
+        /// <param name="node">The node to connect to the right</param>
         public void LinkRight(DancingNode node) {
             node.right = this.right;
             this.right.left = node;
@@ -28,6 +34,10 @@ namespace Omega_Sudoku.src.DancingLinks
             node.left = this;
         }
 
+        /// <summary>
+        /// This method links the given node to the left of the current node
+        /// </summary>
+        /// <param name="node">The node to connect to the right</param>
         public void LinkDown(DancingNode node) {
             node.down = this.down;
             this.down.up = node;
@@ -35,20 +45,36 @@ namespace Omega_Sudoku.src.DancingLinks
             node.up = this;
         }
 
+        /// <summary>
+        /// This method cancels the connections of the 
+        /// left and right nodes to the current node
+        /// </summary>
         public void UnlinkLeftRight() {
             this.right.left = this.left;
             this.left.right = this.right;
         }
 
+        /// <summary>
+        /// This method cancels the connections of the 
+        /// up and down nodes to the current node
+        /// </summary>
         public void UnlinkUpDown() {
             this.down.up = this.up;
             this.up.down = this.down;
         }
 
+        /// <summary>
+        /// This method relinks the current node to the nodes 
+        /// to its right and left
+        /// </summary>
         public void RelinkLeftRight() {
             this.left.right = this.right.left = this;
         }
 
+        /// <summary>
+        /// This method relinks the current node to the nodes
+        /// to its up and down
+        /// </summary>
         public void RelinkUpDown() {
             this.up.down = this.down.up = this;
         }
