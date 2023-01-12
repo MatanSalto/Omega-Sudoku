2dcf78fa02ea98dd3b8fa94c08a80b6476657584
Matan Salto <saltomtn9@gmail.com>
2023-01-12 13:35:52 +0200

    Add documentation

--- src/Sudoku-Solving/SudokuBoard.cs
+++ src/Sudoku-Solving/SudokuBoard.cs
@@ -1,27 +1,37 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Omega_Sudoku.src.Exceptions;
+
 
 namespace Omega_Sudoku.src.SudokuSolving
 {
+    /// <summary>
+    /// This class represents a Sudoku Board object,
+    /// which is responsible for converting the board into an
+    /// excact cover problem
+    /// </summary>
     public class SudokuBoard
     {
         // The 2D array representing the sudoku grid
         private byte[, ] _grid;
         // The size of the board
         private int _size;
-
         // The number of constraints there are
         private const int NUMBER_OF_CONSTRAINTS = 4;
 
-
-        public SudokuBoard(byte[, ] grid, int size) {
+        /// <summary>
+        /// Constructor for the SudokuBoard class
+        /// </summary>
+        /// <param name="grid">The 2D array representing the board</param>
+        /// <param name="size">The size of the board</param>
+        public SudokuBoard(byte[, ] grid, int size) 
+        {
             this._grid = grid;
             this._size = size;   
         }
 
+        /// <summary>
+        /// Thie method validates the board according to the rules of Sudoku
+        /// </summary>
         public void ValidateBoard()
         {
             byte[,] rows = new byte[_size, _size];
@@ -62,9 +72,10 @@ namespace Omega_Sudoku.src.SudokuSolving
             }
         }
 
-        public byte[, ] CreateExactCoverMatrix() {
+        public byte[,] CreateExactCoverMatrix()
+        {
             // Initialize the cover matrix
-            byte [, ] coverMatrix = new byte[_size*_size*_size, _size*_size*NUMBER_OF_CONSTRAINTS];
+            byte[,] coverMatrix = new byte[_size*_size*_size, _size*_size*NUMBER_OF_CONSTRAINTS];
 
             // The current row in the cover matrix
             int currentRow = 0;
@@ -80,12 +91,13 @@ namespace Omega_Sudoku.src.SudokuSolving
 
 
             // Loop through the cells of the grid
-            for (int row = 0; row < _size; row++) {
-                
+            for (int row = 0; row < _size; row++)
+            {
                 // Move the column constraint column back to the start
                 currentColumnConstraintColumn = _size * _size;
 
-                for (int col = 0; col < _size; col++) {
+                for (int col = 0; col < _size; col++)
+                {
 
                     // Get the current value in the grid
                     int value = _grid[row, col];
@@ -94,9 +106,11 @@ namespace Omega_Sudoku.src.SudokuSolving
                     int square = (row / (int)Math.Sqrt(_size)) * (int)Math.Sqrt(_size) + col / (int)Math.Sqrt(_size);
 
                     // Loop through the possible numbers in the current cell
-                    for (byte number = 1; number <= _size; number++) {
+                    for (byte number = 1; number <= _size; number++)
+                    {
                         // Put a 1 only if the value is 0 or the number is the same as the value
-                        if (value == 0 || number == value) {
+                        if (value == 0 || number == value)
+                        {
 
                             // Fill the cell constraint
                             coverMatrix[currentRow, currentCellConstraintColumn] = 1;
@@ -109,18 +123,15 @@ namespace Omega_Sudoku.src.SudokuSolving
 
                             // Fill the square constraint
                             coverMatrix[currentRow, currentBoxConstraintColumn + square * _size + number-1] = 1;
-                        }  
+                        }
                         currentRow++;
                         currentColumnConstraintColumn++;
-
                     }
                     currentCellConstraintColumn++;
                 }
                 currentRowConstraintColumn += _size;
             }
-  
             return coverMatrix;
         }
-
     }
 }   
